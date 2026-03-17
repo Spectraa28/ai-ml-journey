c2ecb02bd25184134a7afca0e6b308c47b507d3d
Spectraa28 <sonuvermacollege@gmail.com>
2026-03-17 08:53:49 +0530

    Day 23: Monitoring -- Monitored for any drift in v14 feature, raised A proper exception for lack in no. of features

--- Week-3/FraudApi/main.py
+++ Week-3/FraudApi/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from fastapi import HTTPException
 from pydantic import BaseModel
 import numpy as  np
 import joblib
@@ -15,16 +16,21 @@ class Transaction(BaseModel):
 
 @app.post("/predict")
 def predict(transaction: Transaction):
+    if len(transaction.features) != 30:
+        raise HTTPException(status_code=400, detail= f"Expected 30 features, got {len(transaction.features)}")
     data  = np.array(transaction.features).reshape(1,-1)
     amount_time = np.array([[data[0][0], data[0][29]]])  # shape (1,2)
     scaled_amount_time = scaler.transform(amount_time)
     data[0][0] = scaled_amount_time[0][0]   # put Time back
     data[0][29] = scaled_amount_time[0][1]  # put Amount back
+    v14 = data[0][14]
+    v14_alert = bool(abs(v14) > 2.88)
     probability = model.predict_proba(data)[0][1]
     return {
     "fraud": bool(float(probability) >= 0.5),
     "probability": float(probability),
-    "confidence": "high" if float(probability) > 0.8 or float(probability) < 0.2 else "low"
+    "confidence": "high" if float(probability) > 0.8 or float(probability) < 0.2 else "low",
+    "v14_alert": v14_alert
     }
 
 
